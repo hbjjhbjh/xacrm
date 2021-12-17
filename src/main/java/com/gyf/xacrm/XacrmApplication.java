e321e272cc1791bcbcb9485d5850397871bc0f35
hbjjhbjh <2652466267@qq.com>
2021-12-17 23:17:13 +0800

    Initial commit

---
+++ src/main/java/com/gyf/xacrm/XacrmApplication.java
@@ -0,0 +1,14 @@
+package com.gyf.xacrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XacrmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XacrmApplication.class, args);
+  }
+
+}
+
---
+++ src/test/java/com/gyf/xacrm/XacrmApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gyf.xacrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XacrmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
+
